e7277657f23975be665802f02d2741f15223baf1
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2018-12-11 16:05:33 +0000

    Added functionality to Order component. Display fish in order and total

--- src/components/App.js
+++ src/components/App.js
@@ -52,7 +52,10 @@ export default class App extends Component {
             ))}
           </ul>
         </div>
-        <Order />
+        <Order 
+          fishes={this.state.fishes} 
+          order={this.state.order} 
+          />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
